e4dda456a97477c59b4361e86c634d770930587d
Ravinshu Makkar <mravinshu@gmail.com>
2025-09-30 16:30:59 +0530

    fixed video player

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3158267197"
+    serviceWorkerVersion: "3229848462"
   }
 });
